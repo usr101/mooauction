b57cacf5ab5655bd306194989fd662a66930f8d8
Mike Deem <lmdeem@gmail.com>
2014-10-19 11:52:14 -0400

    Add new auction code and update listing of auctions.

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -2,15 +2,30 @@ class AuctionsController < ApplicationController
 
 	# Collects all the auctions known to the system.
 	def index 
+		@auctions = Auction.all
 	end
 
 	# Display the new auction form
 	def new
+		@auction = Auction.new
 	end
 
 	# Create a new auction
 	def create
-		redirect_to :auctions
+		@auction = Auction.new(auction_params)
+		
+		if @auction.save
+			redirect_to :auctions
+		else
+			render 'new'
+		end
+
 	end
+
+	private
+
+		def auction_params
+			params.require(:auction).permit(:name)
+		end
 
 end
--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,2 +1,20 @@
 class Auction < ActiveRecord::Base
+	validates :name, presence: true, length: {maximum: 50}
+
+	def total_registered_buyers
+		0
+	end
+
+	def total_buyer_pays
+		0
+	end
+
+	def total_packer_pays
+		0
+	end
+
+	def total_sales
+		total_buyer_pays + total_packer_pays
+	end
+
 end
