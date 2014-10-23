c2c79d5e4cfebbe0794b3f062f6b27e92005cfce
Mike Deem <lmdeem@gmail.com>
2014-10-22 23:47:42 -0400

    Add create buyer form

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -6,4 +6,31 @@ class BuyersController < ApplicationController
 		@buyers = Buyer.where("auction_id = ?", @auction.id)
 	end
 
+	# Bring up the new buyer form.
+	def new
+		@auction = Auction.find(params[:auction_id])
+		@buyer = Buyer.new
+		@buyer.auction = @auction
+	end
+
+	# Create the new buyer.
+	def create
+		@auction = Auction.find(params[:auction_id])
+		@buyer = Buyer.new(buyer_params)
+		@buyer.auction = @auction
+		
+		if @buyer.save
+			redirect_to :auction_buyers
+		else
+			render 'new'
+		end
+		
+	end
+
+	private
+
+		def buyer_params
+			params.require(:buyer).permit(:number, :name)
+		end
+
 end
