fc9c0708aa56da5e0da4bc5adaa2125b64ac923d
Mike Deem <lmdeem@gmail.com>
2014-10-25 23:12:26 +0000

    Add vagrant configuration

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y postgresql
+sudo apt-get install -y git
+sudo apt-get install -y vim
+sudo apt-get install -y screen 
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y nodejs
+sudo apt-get install -y postgresql-server-dev-all
+sudo -u postgres createuser -s vagrant
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+source ~/.bash_profile
+rbenv install 2.1.2
+cd /vagrant
+rbenv local 2.1.2
+gem install bundler
+bundle install
+rake db:create
+rake db:migrate
