72583665a005ca8a6a0d879aa44641b47ca51f47
Mike Deem <lmdeem@gmail.com>
2014-11-13 19:23:38 -0500

    Add unit tests for SellerType

--- app/models/seller_type.rb
+++ app/models/seller_type.rb
@@ -1,2 +1,3 @@
 class SellerType < ActiveRecord::Base
+	validates :name, presence: true, length: {maximum: 50}
 end
--- test/models/seller_type_test.rb
+++ test/models/seller_type_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class SellerTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "Seller Types should not have names longer than 50 characters" do 
+
+		t = SellerType.new
+		t.name = "a" * 51
+		assert_not t.save
+
+	end
+
+	test "SellerType should not have blank name" do
+		t = SellerType.new
+		assert_not t.save
+	end
+
+
 end
