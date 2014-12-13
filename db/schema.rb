408b96c15e7091b3839e2ce3b32a973e451e1fe2
Mike Deem <lmdeem@gmail.com>
2014-12-13 08:42:39 -0500

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124195231) do
+ActiveRecord::Schema.define(version: 20141213132903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,5 +62,15 @@ ActiveRecord::Schema.define(version: 20141124195231) do
   end
 
   add_index "sellers", ["seller_type_id"], name: "index_sellers_on_seller_type_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
