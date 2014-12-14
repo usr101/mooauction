fd9158387dd2ee3fefc7bee6247120957130f387
Mike Deem <lmdeem@gmail.com>
2014-12-13 20:38:00 -0500

    Add new user page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,4 +12,25 @@ class Admin::UsersController < ApplicationController
 		end
   end
 
+  def new 
+    @user = User.new
+  end
+
+  def create
+
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to :admin_users
+    else
+      render 'new'
+    end
+
+  end
+
+  private 
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
