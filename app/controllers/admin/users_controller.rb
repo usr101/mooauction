1c1f626634e6488884cc79495d2b3c1b5591e859
Mike Deem <lmdeem@gmail.com>
2014-12-16 23:29:13 -0500

    Add edit capability to users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -16,6 +16,20 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to :admin_users
+    else 
+      render 'edit'
+    end
+  end
+
   def create
 
     @user = User.create(user_params)
