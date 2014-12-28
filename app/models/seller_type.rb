df9715ef63cc63bc04f3ded51c7a265930f3b5a7
Mike Deem <lmdeem@gmail.com>
2014-12-27 20:34:03 -0500

    Add validations for packercalc and buyercalc

--- app/models/seller_type.rb
+++ app/models/seller_type.rb
@@ -1,5 +1,7 @@
 class SellerType < ActiveRecord::Base
 	validates :name, presence: true, length: {maximum: 50}
+	validates :buyercalc, inclusion: { in: %w(NOCALC WEIGHT) }
+	validates :packercalc, inclusion: { in: %w(NOCALC WEIGHT ) }
 	belongs_to :auction
 	validates :auction, presence: true
 	has_many :sellers, dependent: :destroy
