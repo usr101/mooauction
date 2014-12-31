65d0e5f7b865b114097c8054dbe9e91dae5bee8a
Mike Deem <lmdeem@gmail.com>
2014-12-31 09:49:42 -0500

    Add tests for buyer model

--- test/models/buyer_test.rb
+++ test/models/buyer_test.rb
@@ -16,4 +16,31 @@ class BuyerTest < ActiveSupport::TestCase
      assert_not @buyer.save
   end
 
+  test "buyers require an auction" do
+    @buyer = Buyer.new
+    @buyer.name = "Test Buyer"
+    assert_not @buyer.save
+  end
+
+  test "buyers require a number that is unique to the auction" do
+    buyer = Buyer.new
+    buyer.number = 100
+    buyer.name = "Test Buyer"
+    buyer.auction(auctions(:auction1))
+    buyer.save
+    buyer2 = Buyer.new
+    buyer2.number = 100
+    buyer2.name = "Test Buyer 2"
+    buyer2.auction(auctions(:auction1))
+    assert_not buyer2.save
+  end
+
+  test "pays for seller3" do
+    assert_equal 130, buyers(:buyer3).pays_for_seller(sellers(:seller3))
+  end
+
+  test "pays for seller4" do
+    assert_equal 140, buyers(:buyer4).pays_for_seller(sellers(:seller4))
+  end
+
 end
