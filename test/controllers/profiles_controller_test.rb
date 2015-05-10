190d74fac554e6cbe3d31a08365493cbb6b3d3cf
Mike Deem <lmdeem@gmail.com>
2015-05-09 22:01:19 -0400

    Fix profile controller tests

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
+
+	def setup
+    inject_logon_in_session
+  end
+
   test "should get edit_password" do
     get :edit_password
     assert_response :success
   end
 
   test "should get update_password" do
-    get :update_password
+    get :update_password, change_password_form: {current_password: 'password', new_password: 'password2', confirm_password: 'password2' } 
     assert_response :success
   end
 
