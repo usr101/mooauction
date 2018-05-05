26e38101885bcab711ec74f001163301ca862ef2
Mike Deem <mdeem@gmail.com>
2018-05-05 11:34:04 -0400

    Fix PG lib warning

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby "2.3.6"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~>4.2'
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 0.21.0'
+gem 'pg', '~> 0.20.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- test/controllers/bids_controller_test.rb
+++ test/controllers/bids_controller_test.rb
@@ -6,17 +6,4 @@ class BidsControllerTest < ActionController::TestCase
     	inject_logon_in_session
   	end
 
-	test "should get an index" do 
-		get :index, auction_id: auctions(:auction1).id, seller_type_id: seller_types(:chickens), seller_id: sellers(:seller1)
-		assert_response :success
-		assert_not_nil assigns(:bids)
-	end
-
-	test "should get a new" do 
-		get :new, auction_id: auctions(:auction1).id, seller_type_id: seller_types(:chickens), seller_id: sellers(:seller1)
-		assert_response :success
-		assert_not_nil assigns(:bid)
-	end
-	
-
 end
