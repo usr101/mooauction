e28c71fd71a907ba95b1ef5bcb185b471ab183c7
Mike Deem <lmdeem@gmail.com>
2018-07-04 13:46:05 +0000

    Set up bid relations so that a bid can be created

--- test/models/bid_test.rb
+++ test/models/bid_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class BidTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "valid bid" do
+    bid = Bid.new(buyerbid: 100, option: 1, seller: sellers(:seller1), buyers: [buyers(:buyer1)])
+    assert bid.valid?
+  end
+
+  test "references seller" do
+  end
+
+  test "references buyer" do
+  end
+
+  test "requires buyerbid" do
+  end
+
+  test "requires option" do
+  end
+
 end
