7aa458f715e242b281248dd761217906397e18fe
Mike Deem <lmdeem@gmail.com>
2018-07-21 09:03:35 -0400

    Add record validations to bid.

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,8 +1,9 @@
 class Bid < ActiveRecord::Base
-
 	belongs_to :seller
-  has_many :bidders
+  	has_many :bidders
 	has_many :buyers, through: :bidders
-  validates :seller, presence: true
-
+	validates :seller, presence: true
+	validates :buyers, presence: true
+	validates :buyerbid, numericality: true
+	validates :option, numericality: { only_integer: true }
 end
