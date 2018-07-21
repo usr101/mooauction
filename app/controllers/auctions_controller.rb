c1b266833649fd20b1b1651e3ed73000bea1747f
Mike Deem <lmdeem@gmail.com>
2018-07-21 15:18:37 -0400

    Remove log in error when hitting pages when not logged in.

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -86,7 +86,7 @@ class AuctionsController < ApplicationController
 
 		def logged_in_user
 			unless logged_in?
-				flash[:danger] = "Please log in"
+				#flash[:danger] = "Please log in"
 				redirect_to login_path
 			end
 		end
