f5b6054f576d587bf6400eebcce5571e981eb40c
Mike Deem <lmdeem@gmail.com>
2018-07-21 11:32:01 -0400

    Add next and previous no save links to the edit and add bid pages.

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -18,7 +18,9 @@ class BidsController < ApplicationController
 		@seller_type = SellerType.find(params[:seller_type_id])
 		@buyers = @auction.buyers
 		@bid = Bid.new
-    @bid.seller = @seller
+    	@bid.seller = @seller
+    	@next_seller_path = lookup_next_path
+    	@prev_seller_path = lookup_prev_path	
 	end
 
 	def create
@@ -29,9 +31,9 @@ class BidsController < ApplicationController
 		
 	    if @bid.save
 	    	if params[:commit] == 'next'
-	    		save_next
+	    		redirect lookup_next_path
 			elsif params[:commit] == 'prev'
-				save_prev
+				redirect lookup_prev_path
 			else
 				redirect_to auction_seller_type_bids_path(@auction, @seller_type)
 			end
@@ -47,6 +49,8 @@ class BidsController < ApplicationController
 		@seller_type = SellerType.find(params[:seller_type_id])
 		@bid = @seller.bid
 		@buyers = @auction.buyers
+    	@next_seller_path = lookup_next_path
+    	@prev_seller_path = lookup_prev_path		
 	end
 
 	def update
@@ -58,9 +62,9 @@ class BidsController < ApplicationController
 
 		if @bid.update(bid_params)
 			if params[:commit] == 'next'
-				save_next
+				redirect_to lookup_next_path
 			elsif params[:commit] == 'prev'
-				save_prev
+				redirect_to lookup_prev_path
 			else
 				redirect_to auction_seller_type_bids_path(@auction, @seller_type)
 			end
@@ -70,14 +74,14 @@ class BidsController < ApplicationController
 
 	end
 
-  def destroy
+  	def destroy
 		@auction = Auction.find(params[:auction_id])
 		@seller_type = SellerType.find(params[:seller_type_id])
 		@bid = Bid.find(params[:id])
 		if @bid.destroy
 			redirect_to auction_seller_type_bids_path(@auction, @seller_type)
 		end
-  end
+  	end
 
 	private
 
@@ -86,44 +90,52 @@ class BidsController < ApplicationController
 				.permit(:id, :buyerbid, :option, :seller_id, :buyer_ids => [])
 		end
 
-		def save_next
-			next_seller = @seller_type.sellers.where("sellers.order > ?", @bid.seller.order).order(:order).first
-			if next_seller
-				next_seller_bid = next_seller.bid
-				if next_seller_bid
-					redirect_to edit_auction_seller_type_bid_path(@auction, @seller_type, next_seller_bid, :seller_id => next_seller.id)
-				else 
-					redirect_to new_auction_seller_type_bid_path(@auction, @seller_type, next_seller_bid, :seller_id => next_seller.id)
-				end
-			else
-				first_seller = @seller_type.sellers.order(:order).first
-				first_seller_bid = first_seller.bid
-				if first_seller_bid
-					redirect_to edit_auction_seller_type_bid_path(@auction, @seller_type, first_seller_bid, :seller_id => first_seller.id)
-				else 
-					redirect_to new_auction_seller_type_bid_path(@auction, @seller_type, first_seller_bid, :seller_id => first_seller.id)
-				end
-			end			
+		def lookup_next_seller
+	    	@seller_type.sellers.where("sellers.order > ?", @bid.seller.order).order(:order).first
 		end
 
-		def save_prev
-			prev_seller = @seller_type.sellers.where("sellers.order < ?", @bid.seller.order).order(:order).last
+		def lookup_prev_seller
+			@seller_type.sellers.where("sellers.order < ?", @bid.seller.order).order(:order).last
+		end
+
+		def lookup_prev_path
+			prev_seller = lookup_prev_seller
 			if prev_seller
 				prev_seller_bid = prev_seller.bid
 				if prev_seller_bid
-					redirect_to edit_auction_seller_type_bid_path(@auction, @seller_type, prev_seller_bid, :seller_id => prev_seller.id)
+					edit_auction_seller_type_bid_path(@auction, @seller_type, prev_seller_bid, :seller_id => prev_seller.id)
 				else 
-					redirect_to new_auction_seller_type_bid_path(@auction, @seller_type, prev_seller_bid, :seller_id => prev_seller.id)
+					new_auction_seller_type_bid_path(@auction, @seller_type, prev_seller_bid, :seller_id => prev_seller.id)
 				end
 			else
 				last_seller = @seller_type.sellers.order(:order).last
 				last_seller_bid = last_seller.bid
 				if last_seller_bid
-					redirect_to edit_auction_seller_type_bid_path(@auction, @seller_type, last_seller_bid, :seller_id => last_seller.id)
+					edit_auction_seller_type_bid_path(@auction, @seller_type, last_seller_bid, :seller_id => last_seller.id)
 				else 
-					redirect_to new_auction_seller_type_bid_path(@auction, @seller_type, last_seller_bid, :seller_id => last_seller.id)
+					new_auction_seller_type_bid_path(@auction, @seller_type, last_seller_bid, :seller_id => last_seller.id)
 				end
-			end			
+			end				
+		end
+
+		def lookup_next_path
+			next_seller = lookup_next_seller
+			if next_seller
+				next_seller_bid = next_seller.bid
+				if next_seller_bid
+					edit_auction_seller_type_bid_path(@auction, @seller_type, next_seller_bid, :seller_id => next_seller.id)
+				else 
+					new_auction_seller_type_bid_path(@auction, @seller_type, next_seller_bid, :seller_id => next_seller.id)
+				end
+			else
+				first_seller = @seller_type.sellers.order(:order).first
+				first_seller_bid = first_seller.bid
+				if first_seller_bid
+					edit_auction_seller_type_bid_path(@auction, @seller_type, first_seller_bid, :seller_id => first_seller.id)
+				else 
+					new_auction_seller_type_bid_path(@auction, @seller_type, first_seller_bid, :seller_id => first_seller.id)
+				end
+			end				
 		end
 
 end
